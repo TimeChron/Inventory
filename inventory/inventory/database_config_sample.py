d3aad0d2922567301288e9192e81218edc72bba9
TimeChron <aayushtiwarigit@gmail.com>
2023-07-01 11:21:43 +0530

    sample_config_file corrected

--- inventory/inventory/database_config_sample.py
+++ inventory/inventory/database_config_sample.py
@@ -1,15 +1,19 @@
-## This File is to keep database config
-# Add the required configurations here
+## Here is the sample to configure the database
 
-def configure_database():
-    DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'inventory',
-           'USER': 'chron',
-           'PASSWORD': 'qwerty',
-           'HOST': 'localhost',
-           'PORT': '5433',
-       }
-    }
-    return DATABASES
+"""
+    def configure_database():
+        DATABASES = {
+           'default': {
+               'ENGINE': 'django.db.backends.postgresql',
+               'NAME': 'inventory',
+               'USER': 'chron',
+               'PASSWORD': 'qwerty',
+               'HOST': 'localhost',
+               'PORT': '5433',
+           }
+        }
+        return DATABASES
+"""
+
+## Add this file to database_config
+## And make sure its imported in settings.py
