4e531d56ac45895282c27a53d633f38330dd82e3
TimeChron <aayushtiwarigit@gmail.com>
2023-07-02 17:42:57 +0530

    Static files setting reconfigured

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
